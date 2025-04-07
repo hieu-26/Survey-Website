44ccbd888e3b1bbe6a36e9c836aeeefe02175040
Duongductuan04 <22A1001D0350@students.hou.edu.vn>
2025-04-07 23:24:21 +0700

    thêm giao diện chức năng thêm khảo sát

---
+++ BTL_KhaoSatOnline/Models/ViewModels/SurveyCreateViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BTL_KhaoSatOnline.Models.ViewModels
+
+{
+    public class SurveyCreateViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsPublic { get; set; }
+        public bool AllowMultipleResponses { get; set; }
+        public bool RequireLogin { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public List<QuestionViewModel> Questions { get; set; } = new();
+    }
+
+    public class QuestionViewModel
+    {
+        public string QuestionText { get; set; }
+        public string QuestionType { get; set; } 
+        public bool IsRequired { get; set; }
+        public List<OptionViewModel> Options { get; set; } = new();
+    }
+
+    public class OptionViewModel
+    {
+        public string OptionText { get; set; }
+    }
+}
